40580c367dbe12b1b7bb985d8a5d25dc976f25bb
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:19:08 +0100

    Update a.blade.php

--- onglet/sommaireHeros/a.blade.php
+++ onglet/sommaireHeros/a.blade.php
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Sommaire</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="lineaire-simple"> 
+
+  <h1>Sommaire</h1>
+      <div class="imagedefond7">
+  <ul>
+    <li><a href="/heros/abomination">Abomination</a></li>
+    <li><a href="/heros/absorbingMan">Absorbing Man</a></li>
+    <li><a href="/heros/adamWarlock">Adam Warlock</a></li>
+    <li><a href="/heros/aero">Aero</a></li>
+    <li><a href="/heros/agathaHarkness">Agatha Arkness</a></li>
+    <li><a href="/heros/agent13">Agent 13</a></li>
+    <li><a href="/heros/agentCoulson">Agent Coulson</a></li>
+    <li><a href="/heros/americaChavez">America Chavez</a></li>
+    <li><a href="/heros/angel">Angel</a></li>
+    <li><a href="/heros/angela">Angela</a></li>
+    <li><a href="/heros/antMan">Ant Man</a></li>
+    <li><a href="/heros/apocalypse">Apocalypse</a></li>
+    <li><a href="/heros/armor">Armor</a></li>
+    <li><a href="/heros/arnimZola">Arnim Zola</a></li>
+    <li><a href="/heros/attuma">Attuma</a></th><br><br>
+  </ul>
+</div>
+</div>
+</body>
+
+</html>
