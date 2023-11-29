0ca3508eb3d5947ec425595aeadda513768c98d7
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:12:14 +0100

    Update sommaire.blade.php

--- onglet/sommaire.blade.php
+++ onglet/sommaire.blade.php
@@ -1,1 +1,52 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Sommaire</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="lineaire-simple"> 
+
+  <h1>Sommaire</h1>
+      <div class="imagedefond7">
+  <div class="container">
+  <ul>
+    <th><a href="/onglet/sommaireHeros/a">A</a></th>
+    <th><a href="/onglet/sommaireHeros/b">B</a></th>  
+    <th><a href="/onglet/sommaireHeros/c">C</a></th>
+    <th><a href="/onglet/sommaireHeros/d">D</a></th>
+    <th><a href="/onglet/sommaireHeros/e">E</a></th>
+    <th><a href="/onglet/sommaireHeros/f">F</a></th>
+    <th><a href="/onglet/sommaireHeros/g">G</a></th>
+    <th><a href="/onglet/sommaireHeros/h">H</a></th>
+    <th><a href="/onglet/sommaireHeros/i">I</a></th>
+    <th><a href="/onglet/sommaireHeros/j">J</a></th>
+    <th><a href="/onglet/sommaireHeros/k">K</a></th>
+    <th><a href="/onglet/sommaireHeros/l">L</a></th>
+    <th><a href="/onglet/sommaireHeros/m">M</a></th>
+    <th><a href="/onglet/sommaireHeros/n">N</a></th>
+    <th><a href="/onglet/sommaireHeros/o">O</a></th>
+    <th><a href="/onglet/sommaireHeros/p">P</a></th>
+    <th><a href="/onglet/sommaireHeros/q">Q</a></th>
+    <th><a href="/onglet/sommaireHeros/r">R</a></th>
+    <th><a href="/onglet/sommaireHeros/s">S</a></th>
+    <th><a href="/onglet/sommaireHeros/t">T</a></th>
+    <th><a href="/onglet/sommaireHeros/u">U</a></th>
+    <th><a href="/onglet/sommaireHeros/v">V</a></th>
+    <th><a href="/onglet/sommaireHeros/W">W</a></th>
+    <th><a href="/onglet/sommaireHeros/x">X</a></th>
+    <th><a href="/onglet/sommaireHeros/y">Y</a></th>
+    <th><a href="/onglet/sommaireHeros/z">Z</a></th>
+  </ul>
+ 
+</div>
+</div>
+</div>
+</body>
+
+</html>
