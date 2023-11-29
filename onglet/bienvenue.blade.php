a8ed6d05b18fcad24fd28b70cc9293bd60fb88d2
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:16:42 +0100

    Update bienvenue.blade.php

--- onglet/bienvenue.blade.php
+++ onglet/bienvenue.blade.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Bienvenue</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  
+  <div class="lineaire-simple"> 
+
+  <div class="imagedefond5">
+
+     <ul>
+    <button onclick="window.location.href = 'index.html';"> Continuer </button>    
+</ul>
+  </div>
+    </div>
+</body>
+
+</html>
