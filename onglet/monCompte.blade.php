151f2cf51d71103de27eef4cc032c8af2e0c10e9
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:14:02 +0100

    Update monCompte.blade.php

--- onglet/monCompte.blade.php
+++ onglet/monCompte.blade.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Mon compte</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  
+    <div class="lineaire-simple"> 
+
+  <h1>Mon compte</h1>
+
+<div class="imagedefond4">
+
+<div class="child3">
+  
+<fieldset> 
+
+  <h2>Paramètre</h2> 
+   @if(Auth::check())
+    <h2>Informations personnelles</h2>
+    <p>Nom: {{ Auth::user()->nom }}</p>
+    <p>Prénom: {{ Auth::user()->prenom }}</p>
+    <p>Email: {{ Auth::user()->login }}</p>
+@else
+    <a href="{{ route('seConnecter') }}">Se connecter</a>
+    <a href="{{ route('creerCompte') }}">Créer un compte</a>
+@endif
+
+  </fieldset>
+</div>
+</div>
+    </div>
+</body>
+
+</html>
