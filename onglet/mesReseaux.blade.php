3ab2cbd2f7a8d1e6c295bff95b334d7827167923
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:14:49 +0100

    Update mesReseaux.blade.php

--- onglet/mesReseaux.blade.php
+++ onglet/mesReseaux.blade.php
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Nos réseaux</title>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+</head
+
+  <body>
+
+
+    <div class="lineaire-simple"> 
+<h1>Mes réseaux</h1>
+
+  <div class="imagedefond6">     
+
+  <div class="centrerBoutonReseaux">
+
+<div id="widget ">
+  <div class="btn-o bg" data-scribe="component:button" style="width: 100%;"><a href="https://www.instagram.com/math.is93000?igshid=ZDc4ODBmNjlmNQ==/" class="btn" id="b"><i class="fa fa-instagram ft14"></i> 
+  </div>
+  
+</div>
+  </div>
+</div>
+</body>
+</html>
 
