e4b23b96a98d436df2839f6176be5bac214ba48f
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-29 22:26:33 +0100

    Create connexionControlller.php

---
+++ controllers/connexionControlller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Compte;
+
+class ConnexionController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('onglet.seConnecter');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('login', 'mdp');
+
+        $user = Compte::where('login', $credentials['login'])->first();
+
+        if (!$user || $credentials['mdp'] !== $user->mdp) {
+            return back()->withErrors(['login_error' => 'Identifiants invalides. Veuillez réessayer.']);
+        }
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Bonjour ' . $user->prenom . ' !');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('success', 'Vous avez été déconnecté.');
+    }
+}
