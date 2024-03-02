ad25d4d59e705d8291f18e03da445930678ffaba
mathis <138501804+dev-sio-M@users.noreply.github.com>
2024-03-02 18:18:55 +0100

    Update creerCompteController.php

--- controllers/creerCompteController.php
+++ controllers/creerCompteController.php
@@ -9,28 +9,48 @@ class CreerCompteController extends Controller
 {
     public function showRegistrationForm()
     {
-        return view('onglet.creerCompte'); // Assurez-vous d'avoir votre vue pour le formulaire
+        return view('onglet.creerCompte'); 
     }
 
     public function register(Request $request)
     {
-        // Validation des données entrées par l'utilisateur
-        $validatedData = $request->validate([
-            'nom' => 'required|string|max:255',
-            'prenom' => 'required|string|max:255',
-            'login' => 'required|string|email|max:255|unique:Compte',
-            'mdp' => 'required|string|min:8|confirmed',
-        ]);
+			$validatedData = $request->validate([
+				'nom' => 'required|string|alpha|max:255',
+				'prenom' => 'required|string|alpha|max:255',
+				'login' => ['required', 'string', 'regex:/^[a-z0-9-]+$/','max:255','unique:Compte'],
+				'mdp' => ['required', 'string', 'min:8', 'regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*(_|[^\w])).+$/'],
+			],[
+'nom.required' => 'Le champ :attribute est obligatoire.',
+    'nom.alpha' => 'Le champ :attribute ne doit contenir que des lettres.',
+    'prenom.required' => 'Le champ :attribute est obligatoire.',
+    'prenom.alpha' => 'Le champ :attribute ne doit contenir que des lettres.',
+    'login.required' => 'Le champ :attribute est obligatoire.',
+    'login.regex' => 'Le format du champ :attribute est invalide. Il ne doit contenir que des lettres minuscules, des chiffres ou des tirets.',
+    'login.max' => 'Le champ :attribute ne doit pas dépasser :max caractères.',
+    'login.unique' => 'Le champ :attribute existe déjà.',
+    'mdp.required' => 'Le champ :attribute est obligatoire.',
+    'mdp.min' => 'Le champ :attribute doit comporter au moins :min caractères.',
+    'mdp.regex' => 'Le format du champ :attribute est invalide. Il doit contenir minimum 8 caractères avec au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.',
+], [
+    'nom' => 'nom',
+    'prenom' => 'prénom',
+    'login' => 'login',
+    'mdp' => 'mot de passe',
+				]);
 
-        // Création d'un nouvel utilisateur dans la base de données
-        $user = Compte::create([
-            'nom' => $validatedData['nom'],
-            'prenom' => $validatedData['prenom'],
-            'login' => $validatedData['login'],
-            'mdp' => $validatedData['mdp'], // Cryptage du mot de passe
-        ]);
 
-        // Redirection après l'enregistrement avec un message de succès
-        return redirect('/')->with('success', 'Votre compte a été créé avec succès ! Connectez-vous maintenant.');
+
+        try {
+            $user = Compte::create([
+                'nom' => $validatedData['nom'],
+                'prenom' => $validatedData['prenom'],
+                'login' => $validatedData['login'],
+                'mdp' => $validatedData['mdp'], // Cryptage du mot de passe
+            ]);
+
+            return redirect('/onglet/bienvenue')->with('success', 'Votre compte a été créé avec succès ! Connectez-vous maintenant.');
+        } catch (\Exception $e) {
+            return back()->withInput()->withErrors(['error' => 'La création du compte a échoué. Veuillez réessayer.']);
+        }
     }
 }
